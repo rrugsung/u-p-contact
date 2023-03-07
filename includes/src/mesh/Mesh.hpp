2c008e0d2517db2fa06977763e109000fc8ab883
Chihun Sung <70040297+rrugsung@users.noreply.github.com>
2023-03-06 18:02:55 -0600

    Update Mesh.hpp

--- includes/src/mesh/Mesh.hpp
+++ includes/src/mesh/Mesh.hpp
@@ -2,8 +2,8 @@
                           Material Point Method
                            Shyamini Kularathna
                          University Of Cambridge
-		       	                   Chihun Sung
-		                  University of Texas at Austin
+		       	      Chihun Sung
+	              University of Texas at Austin
 File: Mesh.hpp
 ****************************************************************************/
 
